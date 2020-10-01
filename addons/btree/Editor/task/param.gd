09a7075dead59bad6a5e94ff032941d29da9c147
fian46 <fian.manafe46@gmail.com>
2020-10-01 19:32:31 +0800

    undo redo implementation

--- addons/btree/Editor/task/param.gd
+++ addons/btree/Editor/task/param.gd
@@ -2,25 +2,45 @@ tool
 extends HBoxContainer
 
 signal remove_me(me)
-
-var id = 0
+var float_regex:RegEx = RegEx.new()
 
 func _ready():
+	float_regex.compile("^[-+]?\\d*\\.?\\d*$")
 	$OptionButton.clear()
 	$OptionButton.add_item("Number")
 	$OptionButton.add_item("Text")
 	$OptionButton.selected = 1
+	_on_OptionButton_item_selected(1)
+	$text_value.connect("text_changed", self, "validate")
+	$text_value.connect("focus_exited", self, "fe_validate")
 	return
 
-func set_id(value:int):
-	id = value
+func fe_validate():
+	var value = $text_value.text
+	if  $OptionButton.selected == 0:
+		var r = float_regex.search(value)
+		if  r:
+			$text_value.text = r.get_string()
+		else:
+			$text_value.text = "0"
 	return
 
-func _process(delta):
-	$Label.text = str(id, " : ")
+func validate(value:String):
+	if  $OptionButton.selected == 0:
+		if  value.length() == 0:
+			return
+		var r = float_regex.search(value)
+		if  r:
+			var pc = $text_value.caret_position
+			$text_value.text = r.get_string()
+			$text_value.caret_position = pc
+		else:
+			$text_value.text = "0"
+			$text_value.caret_position = 1
 	return
 
-func _on_TextEdit_gui_input(event):
+func update_label():
+	$Label.text = str(get_index(), " : ")
 	return
 
 func _on_Button_pressed():
@@ -28,39 +48,24 @@ func _on_Button_pressed():
 	return
 
 func get_value():
-	var val = $TextEdit.text
+	var val
 	if  $OptionButton.selected == 0:
-		if  val.is_valid_integer():
-			val = int(val)
-		elif val.is_valid_float():
-			val = float(val)
+		var text:String = $text_value.text
+		if  text.is_valid_integer():
+			val = text.to_int()
 		else:
-			val = 0
+			val = text.to_float()
+	else:
+		val = $text_value.text
 	return [val, $OptionButton.selected]
 
 func set_value(values):
-	$TextEdit.text = str(values[0])
 	$OptionButton.selected = values[1]
+	_on_OptionButton_item_selected(values[1])
+	$text_value.text = str(values[0])
 	return
-
-func _on_TextEdit_focus_exited():
-	if  not valid():
-		$TextEdit.text = "0"
-	return
-
-func valid()->bool:
-	var val = $TextEdit.text
-	if  $OptionButton.selected == 0:
-		if  val.is_valid_integer():
-			return true
-		elif val.is_valid_float():
-			return true
-		else:
-			return false
-	else:
-		return true
 
 func _on_OptionButton_item_selected(ID):
-	if  not valid():
-		$TextEdit.text = "0"
+	if  ID == 0:
+		validate($text_value.text)
 	return
