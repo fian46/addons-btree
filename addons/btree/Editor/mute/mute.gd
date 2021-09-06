6e8e08a17b86420f677a85b9cdb7a5aacfd4d5fd
Ryan <rlinehan2@gmail.com>
2021-09-06 14:11:04 -0700

    Changed mute node to inherit from BehaviorTreeNode

--- addons/btree/Editor/mute/mute.gd
+++ addons/btree/Editor/mute/mute.gd
@@ -1,29 +1,6 @@
 tool
-extends GraphNode
+extends BehaviorTreeNode
 
 const Runtime = preload("res://addons/btree/Runtime/runtime.gd")
 
 const type = Runtime.TNodeTypes.MUTE
-
-func _ready():
-	connect("close_request", self, "close_request")
-	return
-
-func _enter_tree():
-	title = name
-	return
-
-func get_data():
-	return {
-		"size" : rect_size,
-		"offset" : offset
-	}
-
-func set_data(data):
-	rect_size = data.size
-	offset = data.offset
-	return
-
-func close_request():
-	get_parent().child_delete(self)
-	return
