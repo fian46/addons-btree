6910924d1769a26f7de50ce609a4f316b341fc1a
Ryan <rlinehan2@gmail.com>
2021-09-06 14:08:55 -0700

    edited minum node to inherit from behaviortreenode
    unsure what this node does at the moment

--- addons/btree/Editor/minim_node/minim_node.gd
+++ addons/btree/Editor/minim_node/minim_node.gd
@@ -1,35 +1,27 @@
 tool
-extends GraphNode
+extends BehaviorTreeNode
 
 const Runtime = preload("res://addons/btree/Runtime/runtime.gd")
 
 const type = Runtime.TNodeTypes.MINIM
 var data
 
-func _ready():
-	connect("close_request", self, "close_request")
+func _ready():	
 	$expand.connect("pressed", self, "pressed")
 	return
 
 func search_token():
 	return $slot0/label.text
 
-func _enter_tree():
-	title = name
-	return
 
 func get_data():
-	var ret_data = {
-		"offset":offset,
-		"size":rect_size,
-		"data":data,
-		"label": $slot0/label.text
-	}
+	var ret_data = .get_data();
+	ret_data.data = data;
+	ret_data.label = $slot0/label.text;	
 	return ret_data
 
 func set_data(ndata):
-	offset = ndata.offset
-	rect_size = ndata.size
+	.set_data(ndata)	
 	data = ndata.data
 	if  ndata.has("label"):
 		$slot0/label.text = ndata.label
@@ -39,8 +31,4 @@ func set_data(ndata):
 
 func pressed():
 	get_parent().maximize_node(self)
-	return
-
-func close_request():
-	get_parent().child_delete(self)
 	return
